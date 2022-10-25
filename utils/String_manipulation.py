f547c9c3ffeab8f26b2cfd5ea74a7632230680db
RuiVieira89 <rui.vieira.vv@gmail.com>
2022-10-25 16:46:34 +0200

    added exception handling

--- utils/String_manipulation.py
+++ utils/String_manipulation.py
@@ -5,7 +5,11 @@ def WinFolder_path_to_PY(windows_path):
     # This turns Win path \ into python \\
     ## do not forget to input a --> r'string' ##
     
-    path_split = windows_path.split('\\')
-    PY_path = os.path.join("/", "c:", os.sep, *path_split[1:])
+    try:
+        path_split = windows_path.split('\\')
+        PY_path = os.path.join("/", "c:", os.sep, *path_split[1:])
+    except Exception:
+        print('\ndo not forget to input r before string for RAW STRING\n')
+        exit(0)
     
     return PY_path
