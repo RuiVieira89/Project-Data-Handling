e51546094f3a81e34b0c3ef160bbf917a07c499a
RuiVieira89 <rui.vieira.vv@gmail.com>
2022-11-18 20:11:54 +0100

    drafts

--- ProjectDataHandling/GUI/gui_tabs.py
+++ ProjectDataHandling/GUI/gui_tabs.py
@@ -47,7 +47,10 @@ def GUI_tabs(context):
                 
     tabs = []
     for tab in context:
-        tabs.append(return_tab(tab))
+        
+        tabs.append(
+            return_tab(tab)
+            )
     
     tab_group = [[sg.Text('Good day!\nSelect a tab.')],
                  [sg.TabGroup([tabs],
@@ -57,7 +60,8 @@ def GUI_tabs(context):
                         #selected_title_color='Green', 
                         #selected_background_color='Gray', 
                         border_width=5), 
+                  ],
                   sg.Button('Exit')
-                  ]]
+                 ]
     
     return tab_group
