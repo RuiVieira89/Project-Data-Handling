ff669fab66533d6d35cd9bc0a8a3070126d18bae
RuiVieira89 <rui.vieira.vv@gmail.com>
2023-02-03 20:51:11 +0100

    Improved teh Gantt chart. Drop na axis on scraper

--- ProjectDataHandling/project_management/timelines/make_schedule_from_excel.py
+++ ProjectDataHandling/project_management/timelines/make_schedule_from_excel.py
@@ -54,111 +54,69 @@ class Gantt_Schedule:
                 
         return self.c_dict[row['Department']]
     
-    def plot(self, major_time_delta=7):
+    def plot(self):
         
         from matplotlib.patches import Patch
         fig, self.ax = plt.subplots(1, figsize=(16,8))
         # bars
-        self.ax.barh(self.df.Task, self.df.current_num, left=self.df.start_num, color=self.df.color)
-        self.ax.barh(self.df.Task, self.df.days_start_to_end, left=self.df.start_num, color=self.df.color, alpha=0.5)
+        self.ax.barh(self.df.Task, self.df.current_num, left=self.df.Start, color=self.df.color)
+        self.ax.barh(self.df.Task, self.df.days_start_to_end, left=self.df.Start, color=self.df.color, 
+                     alpha=0.5)
         
         # texts
         for idx, row in self.df.iterrows():
             # completion status
-            self.ax.text(row.end_num+0.1, idx, 
+            self.ax.text(mdates.date2num(row.End)+0.1, idx, 
                     f"{int(row.Completion*100)}%", 
                     va='center', alpha=0.8)
             # Task ID
-            self.ax.text(row.start_num+0.1, idx,
+            self.ax.text(mdates.date2num(row.Start)+0.1, idx,
                     f"{row.Task}", 
                     va='center', alpha=0.8,
                     backgroundcolor='white')
-                        
+              
         ##### LEGENDS #####
         #c_dict = {'MKT':'#E64646', 'FIN':'#E69646', 'ENG':'#34D05C', 'PROD':'#34D0C3', 'IT':'#3475D0'}
         legend_elements = [Patch(facecolor=self.c_dict[i], label=i)  for i in self.c_dict]
         plt.legend(handles=legend_elements)
-        ##### TICKS #####
-        xticks = np.arange(0, self.df.end_num.max()+1, major_time_delta)
-        xticks_labels = pd.date_range(self.proj_start, end=self.df.End.max()).strftime("%d/%m/%y")
-        xticks_minor = np.arange(0, self.df.end_num.max()+1, major_time_delta)
-        self.ax.set_xticks(xticks)
-        self.ax.set_xticks(xticks_minor, minor=True)
-        self.ax.set_xticklabels(xticks_labels[::major_time_delta])
-        self.ax.tick_params(axis='x', rotation=90)
+
       
-    def gridlines(self, period='Month', major_time_delta=30):
+    def gridlines(self, period='Month'):
+        
+        # Set the x-axis to display major and minor time steps
+        if period == 'Month':
+            self.ax.xaxis.set_major_locator(mdates.MonthLocator())
+            self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y'))
+            self.ax.xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=mdates.MO))
+        elif period == 'Year':
+            self.ax.xaxis.set_major_locator(mdates.YearLocator())
+            self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%b %Y'))
+            self.ax.xaxis.set_minor_locator(mdates.MonthLocator()) #byweekday=mdates.MO))
+        elif period == 'Day':
+            self.ax.xaxis.set_major_locator(mdates.WeekdayLocator())
+            self.ax.xaxis.set_major_formatter(mdates.DateFormatter('%d %b %Y'))
+            self.ax.xaxis.set_minor_locator(mdates.DayLocator()) #byweekday=mdates.MO))
         
         
-        #ax.xaxis.set_major_locator(MonthLocator(bymonthday=1))
-        #ax.xaxis.set_major_formatter(md.DateFormatter('%b'))
-
-        # grid lines
-        self.ax.set_axisbelow(True)
-        self.ax.xaxis.grid(color='gray', linestyle='dashed', alpha=0.2, which='both')
-
-        # ticks top
-        # create a new axis with the same y
-        ax_top = self.ax.twiny()
-
-        # align x axis
-        self.ax.set_xlim(0, self.df.end_num.max())
-        ax_top.set_xlim(0, self.df.end_num.max())
-
-        # top ticks (markings)
-        xticks_top_minor = np.arange(0,
-                                     self.df.end_num.max()+1, major_time_delta)
-        ax_top.set_xticks(xticks_top_minor, minor=True)
-        # top ticks (label)
-        xticks_top_major = np.arange(0, 
-                                     self.df.end_num.max()+1, major_time_delta)
-        ax_top.set_xticks(xticks_top_major, minor=False)
-                
-        # period labels
-        if period == "week" or period == "Week":
-            xticks_top_labels = [f"{period} {i}" for i in np.arange(1, len(xticks_top_major)+1, 1)]
-        else:
-            xticks_top_labels = []
-            time_stamp = self.df['Start'][0]
-            for i in np.arange(1, len(xticks_top_major)+1, 1):
-                xticks_top_labels.append(
-                    f"{time_stamp.month} / {time_stamp.year}")
-                time_stamp = time_stamp + pd.DateOffset(months=1)
-                
-        ax_top.set_xticklabels(xticks_top_labels, ha='center', minor=False)
-            
-        # hide major tick (we only want the label)
-        ax_top.tick_params(which='major', color='w')
-        # increase minor ticks (to marks the periods start and end)
-        ax_top.tick_params(which='minor', length=major_time_delta, color='k')
-
-        # remove spines
-        self.ax.spines['right'].set_visible(False)
-        self.ax.spines['left'].set_visible(False)
-        self.ax.spines['left'].set_position(('outward', 10))
-        self.ax.spines['top'].set_visible(False)
-
-        ax_top.spines['right'].set_visible(False)
-        ax_top.spines['left'].set_visible(False)
-        ax_top.spines['top'].set_visible(False)
-             
-        ax_top.xaxis.grid(color='black', 
-                           #linestyle='dashed', 
-                           alpha=0.8, 
-                           which='both')
+        self.ax.grid(which='major', axis='x', linestyle='-', color='black', linewidth=1)
+        self.ax.grid(which='minor', axis='x', linestyle='--', color='grey', linewidth=0.5)
+        self.ax.xaxis.set_tick_params(rotation=90, labelsize=20)
+        self.ax.autoscale()
+        self.ax.set_xlim(left=self.df.Start.iloc[0], right=self.df.End.iloc[-1])
+        
+        plt.tight_layout()
 
         
     def today_line(self):
         # Today line
-        today_to_ref_dt = pd.Timestamp.today() - self.df['Start'][0]
         today_line_length = len(self.df)
         # Draw vertical line on today's mark
-        self.ax.vlines(today_to_ref_dt.days, 
+        self.ax.vlines(pd.Timestamp.today(), 
                        ymin=-1, ymax=today_line_length, 
                        color='red', lw=2, zorder=-1) 
-        self.ax.text(today_to_ref_dt.days, -1, 
+        self.ax.text(pd.Timestamp.today(), -1, 
                      pd.Timestamp.today().date(), fontweight='bold')
-        self.ax.text(today_to_ref_dt.days, today_line_length, 
+        self.ax.text(pd.Timestamp.today(), today_line_length, 
                      pd.Timestamp.today().date(), fontweight='bold')
     
         
@@ -171,18 +129,3 @@ class Gantt_Schedule:
 
         if show:
             plt.show()
-
-def main():
-    
-    #from utils.String_manipulation import WinFolder_path_to_PY
-    PATH = r'C:\Users\vieir\OneDrive\Documentos\00_TEST'
-    #WinFolder_path_to_PY(r'C:\Users\vieir\OneDrive\Documentos\00_TEST')
-
-    gantt = Gantt_Schedule(PATH)
-    gantt.plot(7)
-    gantt.gridlines('Month',30)
-    gantt.today_line()
-    gantt.ouput(show=True)
-
-if __name__ == "__main__":
-    main()
